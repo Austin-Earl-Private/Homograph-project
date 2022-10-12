c851ed19dbbc9d3d437203fecd72fb9d6468de79
Austin-Earl-Private <the.qmind2020@gmail.com>
2022-10-11 20:42:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import os
-from pathlib import Path
 import re
 
+
 def generate_path_list_from_string(string):
+    # This function worked as follows
+    # string passed into function as the following format {letter}:{ folder or file }{ // or \ }{ folder or file }
+    # Remove drive and append to list
+    # split on // or \
+    # if spliting element is empty string, ignore it
+    # add to list and return
     parts = []
-    directory_list = re.split('(\\w:)',string)
+    directory_list = re.split('(\\w:)', string)
     parts.append(directory_list[1])
-    files = re.split(r'\\|/',directory_list[2])
+    files = re.split(r'\\|/', directory_list[2])
     for ele in files:
         if ele != "":
             parts.append(ele)
     return parts
 
-def generate_file_flow_list(unprocessed_file_folder_list):
+
+def canonize_file_list(unprocessed_file_folder_list):
+    # process file path list as a stack
+    # folders append
+    # back path ".." pops stack
+    # by the end you should get the conical version of the path
     processed_list = []
     for element in unprocessed_file_folder_list:
         if element == "..":
@@ -27,8 +38,12 @@ def generate_file_flow_list(unprocessed_file_folder_list):
 
 
 def test_homograph(string1, string2):
-    string1_processed = generate_file_flow_list(generate_path_list_from_string(string1))
-    string2_processed = generate_file_flow_list(generate_path_list_from_string(string2))
+    # canonize list
+    # if they are not same length, they are not homographs
+    # parse over list and check element by index to see if matches in other pair
+    # return False if not same, return True if they are same
+    string1_processed = canonize_file_list(generate_path_list_from_string(string1))
+    string2_processed = canonize_file_list(generate_path_list_from_string(string2))
 
     if len(string1_processed) != len(string2_processed):
         return False
@@ -40,12 +55,12 @@ def test_homograph(string1, string2):
     return True
 
 
-
 def run_test(cwd, string1, string2):
-    if(test_homograph(cwd+string1, cwd+string2)):
+    if (test_homograph(cwd + string1, cwd + string2)):
         print(f'"{string1}" and "{string2}" are homographs')
     else:
         print(f'"{string1}" and {string2} are not homographs')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
