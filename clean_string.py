358ff5860682a1ac5fedcfda694a87efb36ccdc6
Hicken-Fredrick <Miroku101@gmail.com>
2022-10-13 19:48:13 -0600

    Finalize Project

--- clean_string.py
+++ clean_string.py
@@ -2,11 +2,22 @@ import re
 
 
 def clean_string(string):
-    dirty_chars = ['<', '>', ':', '"', '|', '?', '*']
+   #define invalid characters in windows set
+    dirty_chars = ['<', '>', ":", '"', '|', '?', '*']
 
+   #save drive letters, remove from string ot be cleaned
+    tempString = string[0:2]
+    string = string[2:]
+
+   #clean string of invalid characters
     for i in dirty_chars:
         string = string.replace(i, '')
 
+   #make sure that the correct number of slashes exist
     cleaned_string = re.sub(r"/{2,}", "/", string)
+   
+   #prepend drive letters back onto cleaned string
+    cleaned_string = tempString + cleaned_string
 
+   #return string
     return cleaned_string
--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def userInputTests():
    #get user inputs to test whether they are homographs
    homoTestString1 = input("Enter First File Path: ")
    homoTestString2 = input("Enter Second File Path: ")
+   #prepend CWD to strings
+   homoTestString1 = os.getcwd() + homoTestString1
+   homoTestString2 = os.getcwd() + homoTestString2
    #Run Individual Test
    result = runTest(homoTestString1, homoTestString2) 
    #print result to users
@@ -26,17 +29,20 @@ These test sets are all arbitrary and don't take into account the users system.
 """
 def runPremadeTestCases():
    #arbitrary working directory for proof of concept
-   cwd = "\\home\\cse453\\week05\\"
+   cwd = "D:\\home\\cse453\\week05\\"
    #all strings within will be homographs to strings within homographStrings list (this list should be longer than non homograph list)
    homographStrings = [f"{cwd}test.txt", f"{cwd}.\\test.txt", f"{cwd}../../../../../../home/cse453/week05/test.txt",
    f"{cwd}..\\week05\\..\\week05\\..\\week05\\test.txt", f"{cwd}..\\week04\\..\\week05\\test.txt", f"{cwd}./test.txt"]
    #all strings will not be homographs to the homographStrings list
-   nonHomographStrings = ["..\\week04\\test.txt"]
+   nonHomographStrings = [f"{cwd}..\\week04\\test.txt", f"{cwd}./NtaHomaGram.txt", f"{cwd}test", 
+   f"{cwd}..\\..\\..\\..\\home\\cs301\\week05\\test.txt"]
 
    #loop through homograph tests using the length of the test cases
    for i in range(0, len(homographStrings)):
       #using i to get element 0 starting at the front of the list and length - (i + 1) to get the end of the list
-      result = runTest(homographStrings[i], homographStrings[len(homographStrings)-(i+1)])
+      result = runTest(homographStrings[i], homographStrings[len(homographStrings)-1-i])
+      print(homographStrings[i])
+      print(homographStrings[len(homographStrings)-1-i])
       if result:
          print("These Two Paths Are Homographs\n")
       else:
@@ -46,6 +52,8 @@ def runPremadeTestCases():
    for i in range(0, len(nonHomographStrings)):
       #using i to get element 0 of both homographString and nonHomographStrings
       result = runTest(homographStrings[i], nonHomographStrings[i])
+      print(homographStrings[i])
+      print(nonHomographStrings[i])
       if result:
          print("These Two Paths Are Homographs\n")
       else:
@@ -58,9 +66,6 @@ def runTest(homoTestString1, homoTestString2):
    #canonicalize use inputs cleansing them for homograph test
    homoTestString1 = clean_string.clean_string(homoTestString1)
    homoTestString2 = clean_string.clean_string(homoTestString2)
-   #prepend CWD to strings
-   homoTestString1 = os.getcwd() + homoTestString1
-   homoTestString2 = os.getcwd() + homoTestString2
    #push strings to homograph function
    return homograph.test_homograph(homoTestString1, homoTestString2)
 
@@ -68,34 +73,33 @@ def runTest(homoTestString1, homoTestString2):
 Main logic, gets user choice for basic menu system using a loop
 to ensure correct pathway choices.
 """
-cwd = os.getcwd()
-print(cwd)
-#Show user choices
-print("1 - Manual String Entry")
-print("2 - Run Automatic Test Cases")
-print("3 - Exit Program")
+if __name__ == '__main__':
+   #Show user choices
+   print("1 - Manual String Entry")
+   print("2 - Run Automatic Test Cases")
+   print("3 - Exit Program")
 
-#Get User Choice | 1= user input | 2= test cases | 3= exit
-while True:
-   try:
-      choice = int(input("Enter Choice: "))
-      if choice==1:
-      #pushes to user input logic
-         print("Manual String Entry")
-         userInputTests()
-      elif choice==2:
-      #pushes to automatic tests logic
-         print("Running Test Cases")
-         runPremadeTestCases()
-      elif choice==3:
-      #exits program
-         print("Exiting")
-         break;
-      else:
-      #catches invalid numbers
-         print("Invalid Choice")
-   #catch exceptions to int casting
-   except ValueError:
-      print("Invalid")
-      continue
+   #Get User Choice | 1= user input | 2= test cases | 3= exit
+   while True:
+      try:
+         choice = int(input("Enter Choice: "))
+         if choice==1:
+         #pushes to user input logic
+            print("Manual String Entry")
+            userInputTests()
+         elif choice==2:
+         #pushes to automatic tests logic
+            print("Running Test Cases")
+            runPremadeTestCases()
+         elif choice==3:
+         #exits program
+            print("Exiting")
+            break;
+         else:
+         #catches invalid numbers
+            print("Invalid Choice")
+      #catch exceptions to int casting
+      except ValueError:
+         print("Invalid")
+         continue
 
